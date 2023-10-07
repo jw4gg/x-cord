695392fa7278fc19fbaeb8778554020d0265f206
jw4gg <38415294+jw4gg@users.noreply.github.com>
2023-10-06 14:14:57 -1000

    Basic sidebar channel layout complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import "./sidebar.css";
+import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add"
+import { Divider } from '@material-ui/core';
+import SidebarChannel from './SidebarChannel';
+
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-        <h2>I am the sidebar</h2>
-
         <div className='sidebar_top'>
-            <h3>JW4GG's Server</h3>
+            <h3>JW4gg's Server</h3>
             <ExpandMoreIcon />
         </div>
 
-        
+        <div className='sidebar_channels'>
+            <div className='sidebar_channelsHeader'>
+                <div className='sidebar_header'>
+                    <ExpandMoreIcon />
+                    <h4>Text Channels</h4>
+                </div>
 
+                <AddIcon className='sidebar_addChannel'/>
+            </div>
+            <div className='sidebar_channelsList'>
+                <SidebarChannel />
+                <SidebarChannel />
+                <SidebarChannel />
+                <SidebarChannel />
+
+            </div>
+        </div>
     </div>
   )
 }
