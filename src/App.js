e97a069a7482926731f45eb9edd8864ddabc3783
jw4gg <38415294+jw4gg@users.noreply.github.com>
2023-10-09 20:23:37 -1000

    login auth started. fixed typos in userSlice

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React, { useEffect } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from './features/userSlice';
 import Login from './Login';
 import { auth } from './firebase';
+import { login, logout } from "./features/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser)
 
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
+        console.log ("user is", authUser);
       if (authUser) {
         //user logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
       } else {
         //user is logged out
       }
